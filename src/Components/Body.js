b89499dac1625da9bcd7d56a86a2470726ee6b9d
Mohsin Murtaza <mohsinmurtaza1402@gmail.com>
2024-06-06 23:04:02 -0500

    fetch resturants data from swiggy live api's

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,13 +1,45 @@
 import ResturantCard from "./ResturantCard";
-import { resList } from "../utils/mockData";
+import { useEffect, useState } from "react";
 const Body = () => {
-  return (
+  const [restList, setRestList] = useState([]);
+  const [allRestList, setAllRestList] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setRestList(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setAllRestList(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return restList.length === 0 ? (
+    <h1>Loading.....</h1>
+  ) : (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter-res">
+        <button
+          className="res-btn"
+          onClick={() => {
+            setRestList(restList.filter((res) => res.info.avgRating > 4.5));
+          }}
+        >
+          Top Rated Resturant
+        </button>
+        <button className="res-btn" onClick={() => setRestList(allRestList)}>
+          All Resturant
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((resturant) => (
+        {restList.map((resturant) => (
           <ResturantCard
-            key={resturant.name}
+            key={resturant.info.id}
             resData={resturant}
           ></ResturantCard>
         ))}
--- src/Components/ResturantCard.js
+++ src/Components/ResturantCard.js
@@ -1,14 +1,21 @@
 const ResturantCard = (props) => {
-  const { name, deliveryTime, description } =
-    props?.resData; /* destructuring object */
+  console.log(props.resData.info);
+  const { cloudinaryImageId, name, avgRating, cuisines, costForTwo, sla } =
+    props?.resData?.info; /* destructuring object */
   return (
     <div className="resturant-card">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1gyA7LpwwPROEDTEOU_9-Y2sI2_P4njObMbOGUDnRPA&s"></img>
+      <img
+        style={{ width: 100, height: 100 }}
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      ></img>
       <h4>{name}</h4>
-      <h4>
-        *<span>{deliveryTime}</span>
-      </h4>
-      <p>{description}</p>
+      <h4>{cuisines.join(",")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla.slaString} minutes</h4>
     </div>
   );
 };
