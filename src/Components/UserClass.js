a9630019eca77b104b8aa2c50d00bc5d62fdca1d
Mohsin Murtaza <mohsinmurtaza1402@gmail.com>
2024-07-19 12:50:31 -0500

    converted class component to functional component

--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -1,19 +1,49 @@
-import { Component } from "react";
+// import { Component } from "react";
 
-class UserClass extends Component {
-  constructor(props) {
-    super(props);
-  }
+import { useEffect, useState } from "react";
 
-  render() {
-    const { name, location, email, image } = this.props;
-    return (
-      <div>
-        <img style={{ width: "100px" }} src={image} />
-        <p>Name : {name}</p>
-        <p>Location : {location}</p>
-      </div>
-    );
-  }
-}
+// class UserClass extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     const { name, location, email, image } = this.props;
+//     return (
+//       <div>
+//         <img style={{ width: "100px" }} src={image} />
+//         <p>Name : {name}</p>
+//         <p>Location : {location}</p>
+//       </div>
+//     );
+//   }
+// }
+// export default UserClass;
+
+const UserClass = () => {
+  const [data, setData] = useState({});
+
+  const fetchData = async () => {
+    try {
+      const data = await fetch("https://api.github.com/users/mohsinmurtazaa");
+      const json = await data.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <img style={{ width: "100px" }} src={data.avatar_url} />
+      <p>Name : {data.name}</p>
+      <p>Location : {data.location}</p>
+    </div>
+  );
+};
+
 export default UserClass;
